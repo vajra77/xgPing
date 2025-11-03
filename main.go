5017e37d744bef1d4df895d8ffbfef63a680e13b
vajra77 <f.ferreri@namex.it>
2025-11-03 12:09:03 +0100

    added output to file

--- main.go
+++ main.go
@@ -12,8 +12,9 @@ import (
 func main() {
 
 	// parse command line arguments
-	src := flag.String("source", "", "Import peers from source")
-	srcFmt := flag.String("format", "", "Format of source for import: json or csv")
+	src := flag.String("input", "", "Input for importing peers' data")
+	srcFmt := flag.String("format", "", "Format of input for import: json or csv")
+	output := flag.String("output", "xgping.csv", "Output filename")
 	ixpId := flag.Int("ixp-id", 1, "Import peers from IXP ID")
 	vlanId := flag.Int("vlan-id", 1, "Import peers from VLAN ID")
 	count := flag.Int("count", 10, "Number of ICMP pings to send")
@@ -31,7 +32,7 @@ func main() {
 		peers, err = ImportCSVPeers(*src)
 	}
 	if err != nil {
-		fmt.Printf("Unable to import peers from source: %s", err)
+		fmt.Printf("Unable to import peers from input: %s\n", err)
 		os.Exit(1)
 	}
 
@@ -39,19 +40,29 @@ func main() {
 
 	for _, peer := range peers {
 		wg.Add(1)
+		fmt.Printf("Pinging peer: %s\n", peer.Name())
 		go peer.Ping(*count, *ttl, &wg)
 		time.Sleep(5 * time.Millisecond)
 	}
 
 	wg.Wait()
-	fmt.Println("PEER;NODE;IPV4;COUNT;RTT_MIN;RTT_MAX;RTT_AVG;RTT_STDEV;LOSS;STATUS")
+
+	outFile, err := os.Create(*output)
+
+	if err != nil {
+		fmt.Printf("Unable to create output file: %s", err)
+		os.Exit(1)
+	}
+
+	_, err = fmt.Fprintln(outFile, "PEER;NODE;IPV4;COUNT;RTT_MIN;RTT_MAX;RTT_AVG;RTT_STDEV;LOSS;STATUS")
+
 	for _, peer := range peers {
 		last := peer.LastSample()
 		status := "OK"
 		if last.Loss() > 0 {
 			status = "WARN"
 		}
-		fmt.Printf("%s;%s;%s;%d;%.2f;%.2f;%.2f;%.2f;%.2f;%s\n", peer.Name(), peer.Node(), peer.V4Address(), *count,
+		_, err = fmt.Fprintf(outFile, "%s;%s;%s;%d;%.2f;%.2f;%.2f;%.2f;%.2f;%s\n", peer.Name(), peer.Node(), peer.V4Address(), *count,
 			last.Min(), last.Max(), last.Avg(), last.StdDev(), last.Loss(), status)
 	}
 }
